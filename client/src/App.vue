9fca6465a19dc2177f1db2780d9cef74624d1979
Дмитрий Данев <ddanev@ivi.ru>
2020-10-22 21:39:25 +0300

    client: add base client template

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="nav">
+    <DonationForm/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import DonationForm from "@/DonationForm.vue";
+
+@Options({
+  components: {
+    DonationForm
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
---
+++ client/src/DonationForm.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <span>"Welcome to Your Vue.js + TypeScript App" </span>
+  </div>
+</template>
